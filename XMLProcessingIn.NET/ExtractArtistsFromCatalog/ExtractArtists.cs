df40941fdafa6d81cd8aa98041a8a0e65aad5c95
Nayaata <n.m.keranova@gmail.com>
2016-10-19 15:37:59 +0300

    Upload demo version of my XMLProcessingIn.NET homework

---
+++ XMLProcessingIn.NET/ExtractArtistsFromCatalog/ExtractArtists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ExtractArtistsFromCatalog
+{
+    class ExtractArtists
+    {
+        static void Main()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../../catalogue.xml");
+
+            XmlNode rootNode = doc.DocumentElement;
+
+            Console.WriteLine("List of all artists in current catalague: ");
+
+            //foreach (XmlNode node in rootNode.ChildNodes)
+            //{
+            //    Console.WriteLine(node["artist"].InnerText);
+            //}
+        }
+    }
+}
